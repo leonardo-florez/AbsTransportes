dfd60c8195dc807e27a0e12e09a15579f0863ed8
leonardo.florez <leonfvillegas@gmail.com>
2022-06-26 18:25:50 -0500

    fix

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './pages/home/home.component';
+import { MaterialModule } from '../shared/material/material.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { HomeComponent } from './pages/home/home.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MaterialModule
   ]
 })
 export class HomeModule { }
